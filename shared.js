6bf77258c5c6c0c82f9dcffcadffd5c7a6de9df9
PigSnout <PigSnoutNSFL@gmail.com>
2022-09-30 02:18:51 -0400

    Update Documentation link name

--- shared.js
+++ shared.js
@@ -4,7 +4,7 @@ var navbar_links = {
     "Single Post Formatter": "single_post_formatter.html",
     "Out of Town Scores Generator": "out_of_town_scores_generator.html",
     "Boxscores Generator": "boxscores_generator.html",
-    "About": "."
+    "Documentation": "."
 }
 
 function generate_navbar_html() {
